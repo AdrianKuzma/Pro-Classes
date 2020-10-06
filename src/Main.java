b4b8863673405e5bed8d59fb2d3f3e50ce54619b
AdrianKuzma <beaversvk@gmail.com>
2020-10-06 10:02:24 +0200

    Ulohy 12,13,14

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,31 @@ class Main{
     public static void main(String[] args) {
         Student student1 = new Student(2,123456, "Sam", "Legionarska 1, 01001 Zilina", 52142);
         Student student2 = new Student(5,456789, "Jhon", "Legionarska 2, 01001 Zilina",12345);
+        Person person1 = new Person("Jhon", 21 ,23000);
+        Person person2 = new Person("Steve", 32 ,40000);
+        Person person3 = new Person("Martin", 16 ,2700);
+        Triangle triangle =  new Triangle(3, 4,5  );
         System.out.println(student1.getName() + " " + student1.getStudentNumber2());
         System.out.println(student1.getStudentNumber() + ", " + student1.getPhoneNumber() + ", " + student1.getAddress());
         System.out.println(student2.getStudentNumber() + ", " + student2.getPhoneNumber() + ", " + student2.getAddress());
+        System.out.println(BudgetTotal(new Person[] {person1,person2,person3}));
+        System.out.println(area(triangle));
+        System.out.println(perimeter(triangle));
     }
+    public static int BudgetTotal(Person[] people) {
+        int a = 0;
+        for (int i = 0; i < people.length; i++) {
+            a = a + people[i].getBudget();
+        }
+        return a;
+    }
+        public static int perimeter(Triangle Per){
+            int per = Per.getA() + Per.getB() + Per.getC();
+            return per;
+        }
+        public static double area (Triangle Ara){
+            int s = perimeter(Ara) / 2;
+            int area = s * (s - Ara.getA()) * (s - Ara.getB()) * (s - Ara.getC());
+            return Math.sqrt(area);
+        }
 }
