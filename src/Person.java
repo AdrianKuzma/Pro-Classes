33c0beb21386376c68ac54eb63290dfaefc5e02e
AdrianKuzma <beaversvk@gmail.com>
2020-10-06 10:03:25 +0200

    Ulohy 12,13,14

---
+++ src/Person.java
@@ -0,0 +1,16 @@
+public class Person {
+    private String name;
+    private int Age;
+    private int budget;
+
+    public Person(String name, int age, int budget) {
+        this.name = name;
+        Age = age;
+        this.budget = budget;
+    }
+
+    public int getBudget() {
+        return budget;
+    }
+}
+
